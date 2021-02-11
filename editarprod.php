912303414150bdf1136f00cbe97a3289636f0d48
Juscelino Campos <celino3x@gmail.com>
2021-02-11 20:34:22 -0300

    update 11022021 p2
    Inclusão de Favicon

--- editarprod.php
+++ editarprod.php
@@ -11,6 +11,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
      <link rel="stylesheet" href="css/estilizar.css">
+     <link rel="shortcut icon" href="favicon.ico" />
    </head>
    <body>
      <div id="tm_cont" class="container">
